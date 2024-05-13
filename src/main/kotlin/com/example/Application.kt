f2711e0d9bdd9d1552c68059326fe7d34f1f7d64
PotaninPM <potaninpm@gmail.com>
2024-05-13 16:29:55 +0300

    rating_DeleteOldEvents

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -8,18 +8,30 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import java.io.FileInputStream
+import java.lang.Math.*
 import java.time.Instant
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 data class User(val place: Int, val points: Int, val userUid: String)
+
+@Serializable
+data class Event(val latitude: String, val longitude: String, val creatoUid: String)
 
 fun main(args: Array<String>) {
     val serviceAccount = FileInputStream("src/main/resources/durable-path-406515-firebase-adminsdk-z8c0i-808a95da6f.json")
@@ -29,7 +41,7 @@ fun main(args: Array<String>) {
         .build()
     FirebaseApp.initializeApp(options)
 
-    embeddedServer(Netty, port = 8080, host = "localhost", module = Application::module).start(wait = true)
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
@@ -96,6 +108,60 @@ fun Application.module() {
             delay(1000 * 3600 * 60 * 24)
         }
     }
+
+    install(Routing) {
+        post("/events") {
+            val json = call.receiveText()
+            val event = Json.decodeFromString<Event>(json)
+
+            val latitude = event.latitude.toDouble()
+            val longitude = event.longitude.toDouble()
+            val createUid = event.creatoUid
+//            val longitude = event.longitude
+//            val creatorUid = event.creatorUid
+            //Рядом с вами создано новое событие!
+
+            //curl -X POST -H "Content-Type: application/json" -d "{\"latitude\":\"55.80095081074492\", \"longitude\":\"37.61738169234994\", \"creatoUid\":\"yourUidHere\"}" http://localhost:8080/events
+            val dbRef_user = FirebaseDatabase.getInstance().getReference("users")
+            dbRef_user.addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot?) {
+
+                }
+
+                override fun onCancelled(error: DatabaseError?) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+            sendNotificationToUser("dE-hvrX_Sjurt3Bvu3utjl:APA91bHAnOvZ18poz40Lllo_45UguYFGyhNedkxKyJX9I409PQSllK8yOQrE-yXOF828eKYTrdmmvxDerQHzhdI-4_B4_BbRwY2B_6d1R9JWN2W9RteqBtpPm1W4WdAGi1p2E-iKW_I0", "$createUid", "Проснись и пой")
+            call.respond(HttpStatusCode.OK, "Notification sent")
+        }
+    }
+
+
+}
+fun distanceBetweenCoordinates(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+    val earthRadius = 6371 // in km
+
+    val dLat = Math.toRadians(lat2 - lat1)
+    val dLon = Math.toRadians(lon2 - lon1)
+
+    val a = sin(dLat / 2) * sin(dLat / 2) +
+            cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
+            sin(dLon / 2) * sin(dLon / 2)
+
+    val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return earthRadius * c
+}
+fun sendNotificationToUser(deviceToken: String, title: String, message: String) {
+    val message = Message.builder()
+        .putData("title", title)
+        .putData("message", message)
+        .setToken(deviceToken)
+        .build()
+
+    val response = FirebaseMessaging.getInstance().sendAsync(message).get()
+    println("Sent message: $response")
 }
 
 suspend fun deleteOldEvents() {
